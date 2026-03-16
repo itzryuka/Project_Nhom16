782f73c262b8fe797dbc04b9c9eded36422904cd
Quang Lùn <quangspinner12@gmail.com>
2026-03-16 20:52:10 +0700

    Delete wwwroot directory

--- wwwroot/js/site.js
+++
@@ -1,6 +0,0 @@
-﻿var myCarousel = document.querySelector('#heroBanner')
-var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 10000, // 10000ms = 10 giây
-    wrap: true,      // Hết ảnh thì quay lại từ đầu
-    pause: 'hover'   // Di chuột vào thì tạm dừng để khách xem
-})
